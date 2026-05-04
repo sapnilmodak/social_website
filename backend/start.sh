30d8c1c3f1f64bb1240cc3b11006ec816a6888b6
Harry9021 <harryingle00@gmail.com>
2026-05-05 01:08:02 +0530

    db automation

---
+++ backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Generate Prisma Client (in case it wasn't done in build)
+npx prisma generate
+
+# Push the schema to the database
+# This will create tables if they don't exist
+npx prisma db push --accept-data-loss
+
+# Start the application
+npm run dev
